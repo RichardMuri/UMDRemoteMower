88cf25714d6cf508ca38c5343577a5ace56b0ad8
RichardMuri <rmuri@umassd.edu>
2017-04-11 23:31:03 -0400

    Fixed daemon sort of- it's still tempermental

--- RPi/pwm_control_init.sh
+++ RPi/pwm_control_init.sh
@@ -17,19 +17,64 @@
 # This file needs to be placed in /etc/init.d/ to 
 # run on start up for the Raspberry Pi
 
-case "$1" in 
-    start)
-        echo "Starting pwm_control\n"
-		/home/pwm_control
-        ;;
-    stop)
-        echo "Stopping pwm_control"
-        killall pwm_control
-        ;;
-    *)
-        echo "Usage: /etc/init.d/lawnmower_control start|stop"
-        exit 1
-        ;;
+echo "This is before the case statement\n"
+
+SCRIPT=pwm_control
+RUNAS=root
+
+PIDFILE=/var/run/pwm_control.pid
+LOGFILE=/var/log/pwm_control.log
+
+start(){
+	if [ -f "$PIDFILE" ] && kill -0 $(cat "$PIDFILE"); then
+    echo 'Service already running' >&2
+    return 1
+  fi
+  echo 'Starting service…' >&2
+  local CMD="$SCRIPT &> \"$LOGFILE\" & echo \$!"
+  su -c "$CMD" $RUNAS > "$PIDFILE"
+  echo 'Service started' >&2
+}
+
+stop() {
+  if [ ! -f "$PIDFILE" ] || ! kill -0 $(cat "$PIDFILE"); then
+    echo 'Service not running' >&2
+    return 1
+  fi
+  echo 'Stopping service…' >&2
+  kill -15 $(cat "$PIDFILE") && rm -f "$PIDFILE"
+  echo 'Service stopped' >&2
+}
+
+uninstall() {
+  echo -n "Are you really sure you want to uninstall this service? That cannot be undone. [yes|No] "
+  local SURE
+  read SURE
+  if [ "$SURE" = "yes" ]; then
+    stop
+    rm -f "$PIDFILE"
+    echo "Notice: log file is not be removed: '$LOGFILE'" >&2
+    update-rc.d -f <NAME> remove
+    rm -fv "$0"
+  fi
+}
+
+case "$1" in
+  start)
+    start
+    ;;
+  stop)
+    stop
+    ;;
+  uninstall)
+    uninstall
+    ;;
+  restart)
+    stop
+    start
+    ;;
+  *)
+    echo "Usage: $0 {start|stop|restart|uninstall}"
 esac
 
 exit 0
